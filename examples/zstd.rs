bdc4fbf4b5a962a76af3267e3a1b0268ea315b03
Tom Fay <tomfay@microsoft.com>
2024-10-01 15:09:50 +0100

    allow users to get zstdlayerwriter directly
    Signed-off-by: Tom Fay <tomfay@microsoft.com>

--- examples/zstd.rs
+++ examples/zstd.rs
@@ -5,7 +5,7 @@ fn main() {
     use oci_spec::image::Platform;
     use ocidir::{cap_std::fs::Dir, new_empty_manifest, OciDir};
     let dir = Dir::open_ambient_dir(env::temp_dir(), ocidir::cap_std::ambient_authority()).unwrap();
-    let oci_dir = OciDir::ensure(&dir).unwrap();
+    let oci_dir = OciDir::ensure(dir).unwrap();
 
     let mut manifest = new_empty_manifest().build().unwrap();
     let mut config = ocidir::oci_spec::image::ImageConfigurationBuilder::default()
@@ -13,23 +13,28 @@ fn main() {
         .unwrap();
 
     // Add the src as a layer
-    let mut writer = oci_dir.create_layer_zstd(Some(0)).unwrap();
-    writer
+    let writer = oci_dir.create_layer_zstd(Some(0)).unwrap();
+    let mut builder = tar::Builder::new(writer);
+    builder.follow_symlinks(false);
+
+    builder
         .append_dir_all(".", PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("src"))
         .unwrap();
 
-    let layer = writer.into_inner().unwrap().complete().unwrap();
+    let layer = builder.into_inner().unwrap().complete().unwrap();
     oci_dir.push_layer(&mut manifest, &mut config, layer, "src", None);
 
     // Add the examples as a layer, using multithreaded compression
-    let mut writer = oci_dir.create_layer_zstd_multithread(Some(0), 4).unwrap();
-    writer
+    let writer = oci_dir.create_layer_zstd_multithread(Some(0), 4).unwrap();
+    let mut builder = tar::Builder::new(writer);
+    builder.follow_symlinks(false);
+    builder
         .append_dir_all(
             ".",
             PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("examples"),
         )
         .unwrap();
-    let layer = writer.into_inner().unwrap().complete().unwrap();
+    let layer = builder.into_inner().unwrap().complete().unwrap();
     oci_dir.push_layer(&mut manifest, &mut config, layer, "examples", None);
 
     println!(
